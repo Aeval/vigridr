5d45d8eaebe14ececf22091f7666ede14cb65eee
Zackary Fisher <zfisher@thorlabs.com>
2018-11-05 16:48:07 -0500

    first commit

---
+++ public/javascripts/global.js
@@ -0,0 +1,28 @@
+var userData = [];
+
+//On page ready
+$(document).ready(function(){
+    populateGames();
+})
+
+function populateGames(){
+    var gameContent = '';
+
+    $.getJSON('/users/games', function(data){
+        //For each game create a card
+        $.each(data, function(){
+            gameContent += '<div class="card text-white bg-dark border-secondary">';
+            gameContent += '<img class="card-img-top" src="'+ this.pic +'" height="150px" width="400px" alt="'+ this.game +'">';
+            gameContent += '<div class="card-body">';
+            gameContent += '<h5 class="card-title text-center">'+ this.game +'</h5>';
+            gameContent += '<p class="card-text">'+ this.desc +'</p>';
+            gameContent += '</div>';
+            gameContent += '<div class="card-footer text-center">';
+            gameContent += '<button type="button" class="btn btn-primary">Vote!</a>';
+            gameContent += '</div>';
+            gameContent += '</div>';
+        });
+
+        $('#gameDisp').html(gameContent);
+    })
+}
---
+++ routes/users.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET userlist. */
+router.get('/users', function(req, res) {
+  var db = req.db;
+  var collection = db.get('users');
+  collection.find({},{},function(e,docs){
+    res.json(docs);
+  });
+});
+
+router.get('/games', function(req, res) {
+  var db = req.db;
+  var collection = db.get('games');
+  collection.find({},{},function(e,docs){
+    res.json(docs);
+  });
+});
+
+module.exports = router;
