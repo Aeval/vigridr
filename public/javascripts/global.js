88e1d430800f188fee982ffe12968be70b20cb56
AevalAnkou <ankouaeval@gmail.com>
2018-11-26 12:36:27 -0500

    changed modal library from SA2 to Izi

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -3,20 +3,104 @@ var userData = [];
 //On page ready
 $(document).ready(function(){
     checkUser();
-});
 
-$('#log').on('click', function(){
-    getLogValues();
-});
+    var elems = $('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'top',
+      hoverEnabled: false
+    });    
+})
+//Reg new user, if not exists, then log them in
+$("form[name=regForm]").submit(function(e){
+    e.preventDefault();
 
-$('#reg').on('click', function(){
-    getRegValues();
-});
+    if ($('#regPassword').val() !== $('#regConfirmPassword').val()){
+        iziToast.warning({
+            title: 'Hold on a sec...',
+            message: 'Please make sure your passwords match!',
+            position: 'bottomCenter',
+            overlay: true,
+            timeout: 2000,
+            overlayClose: true
+        });
+        return false
+    }
+    
+    var username = $('#regUserName').val(),
+        email = $('#regEmail').val(),
+        firstname = $('#regFirstName').val(),
+        lastname = $('#regLastName').val(),
+        company = $('#regCompany').val(),
+        password = $('#regPassword').val(),
+        bnet = $('#regBattlenet').val(),
+        steam = $('#regSteamid').val()
 
-$('#chart').on('click', function(){
+    var userReg = {
+        username: username,
+        email: email,
+        firstname: firstname,
+        lastname: lastname,
+        company: company,
+        password: password,
+        bnet: bnet,
+        steam: steam
+    }
+    //Check if user exists and if not, create them and log them in
+    $.ajax({
+        type: 'GET',
+        url: '/users/getuser/'+ username,
+        dataType: 'JSON'
+    }).done(function(response) {
+        if (response.length !== 0){
+            iziToast.error({
+                title: 'Unfortunately...',
+                message: 'That username has been taken already!',
+                position: 'topCenter'
+            });
+            return false;
+        }else{
+            addUser(userReg);
+            var user = {
+                username: username,
+                password: password
+            };
+            $('#modal-reg').iziModal('close');
+            logIn(user);
+        }  
+    })
+})
+//Log in user
+$("form[name=logForm]").submit(function(e){
+    e.preventDefault();
+    
+    var user = {
+        username: $('#logUserName').val(),
+        password: $('#logPassword').val(),
+
+    }
+
+    logIn(user);
+})
+//Open Log in modal
+$('#log').on('click', function(e){
+    /* getLogValues(); */
+    e.preventDefault();
+    $('#modal-login').iziModal('open');
+});
+//Open Reg modal
+$('#reg').on('click', function(e){
+    /* getRegValues(); */
+    e.preventDefault();
+    $('#modal-reg').iziModal('open');
+});
+//Open chart display modal
+$('#chart').on('click', function(e){
+    /* getChart(); */
+    e.preventDefault();
     getChart();
+    $('#getChart').iziModal('open');
 });
-
+//All "hover" functions enable each button to pulse on hover
 $('#chart').hover(function() {
     $(this).addClass('pulse');
 }, function() {
@@ -29,159 +113,14 @@ $('#discord').hover(function() {
     $(this).removeClass('pulse');
 });
 
-function populateGames(){
-    var gameContent = '';
-    var user = Cookies.getJSON('user');
-    var gamevotes = user.votes;
+$('#donate').hover(function() {
+    $(this).addClass('pulse');
+}, function() {
+    $(this).removeClass('pulse');
+});
 
-    $.getJSON('/games/games', function(data){
-        //For each game create a card
-        $.each(data, function(){
-            var gameid = this.gameid;
-
-            gameContent += '<div class="card hoverable small white-text blue-grey darken-3">';
-            gameContent += '<div class="card-image">';
-            gameContent += '<img src="'+ this.pic +'" height="150px" width="400px" alt="'+ this.game +'"></img>';
-            gameContent += '</div>';
-            gameContent += '<div class="card-fab">';
-            if(gamevotes){
-                if(gamevotes[gameid] > 0){
-                    gameContent += '<button id="'+ gameid +'_add" class="btn-floating halfway-fab waves-effect waves-light red scale-transition scale-out"><i class="material-icons">how_to_vote</i></button><button id="'+ gameid +'_remove" class="btn-floating halfway-fab waves-effect waves-light green scale-transition"><i class="material-icons">check</i></button>';
-                }else{
-                    gameContent += '<button id="'+ gameid +'_add" class="btn-floating halfway-fab waves-effect waves-light red scale-transition"><i class="material-icons">how_to_vote</i></button><button id="'+ gameid +'_remove" class="btn-floating halfway-fab waves-effect waves-light green scale-transition scale-out"><i class="material-icons">check</i></button>';
-                }
-            }else{
-                gameContent += '<button id="'+ gameid +'_add" class="btn-floating halfway-fab waves-effect waves-light red scale-transition"><i class="material-icons">how_to_vote</i></button><button id="'+ gameid +'_remove" class="btn-floating halfway-fab waves-effect waves-light green scale-transition scale-out"><i class="material-icons">check</i></button>';
-            }
-            gameContent += '</div>';
-            gameContent += '<div class="card-content">';
-            gameContent += '<p>'+ this.desc +'</p>';
-            gameContent += '</div>';
-            gameContent += '</div>';
-        });
-
-        $('#gameDisp').html(gameContent);
-
-        //Pulse on hover
-        $('.card').hover(function() {
-            $(this).children('.card-fab').children('.btn-floating').addClass('pulse');
-        }, function() {
-            $(this).children('.card-fab').children('.btn-floating').removeClass('pulse');
-        });
-        
-        //Vote to check and back, also include db logic here:
-        $('.btn-floating').on('click', function(){
-            var game = $(this).attr('id')
-            var gameid = $(this).attr('id').split('_')[0]
-            var user = Cookies.getJSON('user');
-            let button = $(this)[0];
-            var vote;
-            var header = {
-                Authorization: user.token
-            };
-            
-
-            if(game.indexOf('add') >= 0){
-
-                vote = {
-                    user: user.user,
-                    gameid: gameid,
-                    vote: 1
-                };
-
-                $.ajax({
-                    type: 'POST',
-                    url: '/games/submitVote/',
-                    headers: header,
-                    data: vote,
-                    dataType: 'JSON'
-                }).done(function(resp){
-                    if(resp.msg !== ''){
-                        toast({
-                            type: 'error',
-                            title: 'We couldn\'t process that vote.'
-                        });
-                    }else{
-                        $(button).addClass('scale-out');
-                        $(button).siblings('.btn-floating').removeClass('scale-out');
-                    }
-                }).fail(function(){
-                    toast({
-                        type: 'error',
-                        title: 'Voting Failed!'
-                    });
-                })
-            }
-
-            if(game.indexOf('remove') >= 0){
-
-
-                vote = {
-                    user: user.user,
-                    gameid: gameid,
-                    vote: -1
-                };
-
-                $.ajax({
-                    type: 'POST',
-                    url: '/games/submitVote/',
-                    headers: header,
-                    data: vote,
-                    dataType: 'JSON'
-                }).done(function(resp){
-                    if(resp.msg !== ''){
-                        toast({
-                            type: 'error',
-                            title: 'We couldn\'t process that vote.'
-                        });
-                    }else{
-                        $(button).addClass('scale-out');
-                        $(button).siblings('.btn-floating').removeClass('scale-out');
-                    }
-                }).fail(function(){
-                    toast({
-                        type: 'error',
-                        title: 'We couldn\'t process that vote.'
-                    });
-                })
-            }
-        });
-    })
-}
-
-function populateGamesList(){
-    var gameContent = '';
-
-    $.getJSON('/games/games', function(data){
-        //For each game create a card
-        $.each(data, function(){
-            gameContent += '<div class="card hoverable small white-text blue-grey darken-3">';
-            gameContent += '<div class="card-image">';
-            gameContent += '<img src="'+ this.pic +'" height="150px" width="400px" alt="'+ this.game +'"></img>';
-            gameContent += '</div>';
-            gameContent += '<div class="card-content">';
-            gameContent += '<p>'+ this.desc +'</p>';
-            gameContent += '</div>';
-            gameContent += '</div>';
-        });
-
-        $('#gameDisp').html(gameContent);
-    })
-}
-
-function checkUser(){
-    var userCookie = Cookies.getJSON('user');
-    
-    if(userCookie){
-        populateGames();
-        $('#voteText').html('<h5 class="white-text center-align">Vote for the games you want to see in the League!</h5>');
-        $('#btns').html(`<a id="logOut" class="waves-effect waves-light red btn"><i class="material-icons left">account_circle</i>${userCookie.user}</a>`);
-        
-        $('#logOut').on('click', function(){
-            logOutUser();
-        })
-    }else{
-        populateGamesList();
-        $('#voteText').html('<h5 class="white-text center-align">Log in to vote on which games to play!</h5>');
-    }
-}
+$('#info').hover(function() {
+    $(this).addClass('pulse');
+}, function() {
+    $(this).removeClass('pulse');
+});
--- public/javascripts/helpers.js
+++ public/javascripts/helpers.js
@@ -1,28 +1,310 @@
+//Check if user has a valid cookie
+function checkUser() {
+  var userCookie = Cookies.getJSON('user');
+
+  if (userCookie) {
+    populateGames();
+    $('#voteText').html('<h5 class="white-text center-align">Vote for the games you want to see in the League!</h5>');
+    $('#btns').html('<a id="logOut" class="waves-effect waves-light red btn"><i class="material-icons left">account_circle</i>' + userCookie.user + '</a>');
+
+    $('#logOut').on('click', function () {
+      logOutUser();
+    })
+  } else {
+    populateGamesList();
+    $('#voteText').html('<h5 class="white-text center-align">Log in to vote on which games to play!</h5>');
+  }
+}
+
 // Add User
-function addUser(regValues) {
-
-    var newUser = {
-      'username': regValues[0],
-      'email': regValues[5],
-      'firstname': regValues[1],
-      'lastname': regValues[2],
-      'company': regValues[6],
-      'password': regValues[7],
-      'bnet': regValues[3],
-      'steam': regValues[4],
-
+function addUser(newUser) {
+  // Use AJAX to post the object to our adduser service
+  $.ajax({
+    type: 'POST',
+    data: newUser,
+    url: '/users/adduser',
+    dataType: 'JSON'
+  }).done(function (response) {
+    // Check for successful (blank) response
+    if (response.msg !== '') {
+      iziToast.error({
+        title: 'Oops!',
+        message: 'We had an issue adding you to the roster! Try again or contact us!',
+    });
     }
+  });
+};
 
-    // Use AJAX to post the object to our adduser service
-    $.ajax({
-      type: 'POST',
-      data: newUser,
-      url: '/users/adduser',
-      dataType: 'JSON'
-    }).done(function( response ) {
-      // Check for successful (blank) response
-      if (response.msg !== '') {
-        swal('Error!',response.msg,'error');
+
+//Function to log in users
+function logIn(user) {
+  $.ajax({
+    type: 'POST',
+    url: '/users/login/',
+    data: user,
+    dataType: 'JSON'
+  }).done(function (response) {
+    if (response.message !== 'Auth successful!') {
+      iziToast.error({
+        title: 'Sorry!',
+        message: 'There was an issue logging in! Please try again!',
+      });
+      return false;
+    } else {
+      $("#modal-login").iziModal('close');
+      iziToast.success({
+        title: 'Welcome!',
+        message: 'Good to see you, ' + response.user + '!',
+        position: 'topCenter'
+      });
+      Cookies.set('user', { token: response.token, user: response.user, votes: response.votes }, { expires: 1 });
+      checkUser();
+    }
+  })
+}
+
+//Populate games for voting (Logged In) and voting logic
+function populateGames() {
+  var gameContent = '';
+  var user = Cookies.getJSON('user');
+  var gamevotes = user.votes;
+  $.getJSON('/games/games', function (data) {
+    //For each game create a card
+    $.each(data, function () {
+      var gameid = this.gameid;
+
+      gameContent += '<div class="card hoverable small white-text blue-grey darken-3">';
+      gameContent += '<div class="card-image">';
+      gameContent += '<img src="' + this.pic + '" height="150px" width="400px" alt="' + this.game + '"></img>';
+      gameContent += '</div>';
+      gameContent += '<div class="card-fab">';
+      if (gamevotes) {
+        if (gamevotes[gameid] > 0) {
+          gameContent += '<button id="' + gameid + '_add" class="btn-floating halfway-fab waves-effect waves-light red scale-transition scale-out"><i class="material-icons">how_to_vote</i></button><button id="' + gameid + '_remove" class="btn-floating halfway-fab waves-effect waves-light green scale-transition"><i class="material-icons">check</i></button>';
+        } else {
+          gameContent += '<button id="' + gameid + '_add" class="btn-floating halfway-fab waves-effect waves-light red scale-transition"><i class="material-icons">how_to_vote</i></button><button id="' + gameid + '_remove" class="btn-floating halfway-fab waves-effect waves-light green scale-transition scale-out"><i class="material-icons">check</i></button>';
+        }
+      } else {
+        gameContent += '<button id="' + gameid + '_add" class="btn-floating halfway-fab waves-effect waves-light red scale-transition"><i class="material-icons">how_to_vote</i></button><button id="' + gameid + '_remove" class="btn-floating halfway-fab waves-effect waves-light green scale-transition scale-out"><i class="material-icons">check</i></button>';
+      }
+      gameContent += '</div>';
+      gameContent += '<div class="card-content">';
+      gameContent += '<p>' + this.desc + '</p>';
+      gameContent += '</div>';
+      gameContent += '</div>';
+    });
+
+    $('#gameDisp').html(gameContent);
+
+    //Pulse on hover
+    $('.card').hover(function () {
+      $(this).children('.card-fab').children('.btn-floating').addClass('pulse');
+    }, function () {
+      $(this).children('.card-fab').children('.btn-floating').removeClass('pulse');
+    });
+
+    //Vote to check and back, also include db logic here:
+    $('.btn-floating').on('click', function () {
+      var game = $(this).attr('id')
+      var gameid = $(this).attr('id').split('_')[0]
+      var user = Cookies.getJSON('user');
+      var button = $(this)[0];
+      var vote;
+      var header = {
+        Authorization: user.token
+      };
+
+
+      if (game.indexOf('add') >= 0) {
+
+        vote = {
+          user: user.user,
+          gameid: gameid,
+          vote: 1
+        };
+
+        $.ajax({
+          type: 'POST',
+          url: '/games/submitVote/',
+          headers: header,
+          data: vote,
+          dataType: 'JSON'
+        }).done(function (resp) {
+          if (resp.msg !== '') {
+            iziToast.error({
+              title: 'Oh no!',
+              message: 'We weren\'t able to process that vote!',
+            });
+          } else {
+            $(button).addClass('scale-out');
+            $(button).siblings('.btn-floating').removeClass('scale-out');
+          }
+        }).fail(function () {
+          iziToast.error({
+            title: 'Oh no!',
+            message: 'We weren\'t able to process that vote!',
+          });
+        })
+      }
+
+      if (game.indexOf('remove') >= 0) {
+
+
+        vote = {
+          user: user.user,
+          gameid: gameid,
+          vote: -1
+        };
+
+        $.ajax({
+          type: 'POST',
+          url: '/games/submitVote/',
+          headers: header,
+          data: vote,
+          dataType: 'JSON'
+        }).done(function (resp) {
+          if (resp.msg !== '') {
+            iziToast.error({
+              title: 'Oh no!',
+              message: 'We weren\'t able to process that vote!',
+            });
+          } else {
+            $(button).addClass('scale-out');
+            $(button).siblings('.btn-floating').removeClass('scale-out');
+          }
+        }).fail(function () {
+          iziToast.error({
+            title: 'Oh no!',
+            message: 'We weren\'t able to process that vote!',
+          });
+        })
       }
     });
-};
+  })
+}
+
+//Populate games when not logged in (no voting)
+function populateGamesList() {
+  var gameContent = '';
+
+  $.getJSON('/games/games', function (data) {
+    //For each game create a card
+    $.each(data, function () {
+      gameContent += '<div class="card hoverable small white-text blue-grey darken-3">';
+      gameContent += '<div class="card-image">';
+      gameContent += '<img src="' + this.pic + '" height="150px" width="400px" alt="' + this.game + '"></img>';
+      gameContent += '</div>';
+      gameContent += '<div class="card-content">';
+      gameContent += '<p>' + this.desc + '</p>';
+      gameContent += '</div>';
+      gameContent += '</div>';
+    });
+
+    $('#gameDisp').html(gameContent);
+  })
+}
+
+//Get votes chart
+function getChart() {
+
+  $.ajax({
+    Type: 'GET',
+    url: '/games/getChart/',
+    dataType: 'JSON'
+  }).done(function (resp) {
+
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: [resp[0].name, resp[1].name, resp[2].name, resp[3].name, resp[4].name, resp[5].name, resp[6].name, resp[7].name, resp[8].name, resp[9].name, resp[10].name, resp[11].name],
+        datasets: [{
+          label: '# of Votes',
+          data: [resp[0].votes, resp[1].votes, resp[2].votes, resp[3].votes, resp[4].votes, resp[5].votes, resp[6].votes, resp[7].votes, resp[8].votes, resp[9].votes, resp[10].votes, resp[11].votes],
+          backgroundColor: [
+            'rgba(255, 0, 0, 0.8)',
+            'rgba(255, 96, 0, 0.8)',
+            'rgba(255, 192, 0, 0.8)',
+            'rgba(255, 255, 0, 0.8)',
+            'rgba(128, 255, 0, 0.8)',
+            'rgba(0, 255, 0, 0.8)',
+            'rgba(0, 255, 224, 0.8)',
+            'rgba(0, 125, 255, 0.8)',
+            'rgba(0, 0, 255, 0.8)',
+            'rgba(96, 0, 255, 0.8)',
+            'rgba(224, 0, 255, 0.8)',
+            'rgba(255, 0, 128, 0.8)'
+          ],
+          borderColor: [
+            'rgba(255, 0, 0, 0.8)',
+            'rgba(255, 96, 0, 0.8)',
+            'rgba(255, 192, 0, 0.8)',
+            'rgba(255, 255, 0, 0.8)',
+            'rgba(128, 255, 0, 0.8)',
+            'rgba(0, 255, 0, 0.8)',
+            'rgba(0, 255, 224, 0.8)',
+            'rgba(0, 125, 255, 0.8)',
+            'rgba(0, 0, 255, 0.8)',
+            'rgba(96, 0, 255, 0.8)',
+            'rgba(224, 0, 255, 0.8)',
+            'rgba(255, 0, 128, 0.8)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        legend: {
+          display: false
+        }
+      }
+    });
+  })
+}
+
+//Confirm reg passwords match
+var password = $("#regPassword")[0];
+var confirm_password = $("#regConfirmPassword")[0];
+//Password validation on keyup
+password.onchange = validatePassword();
+confirm_password.onkeyup = validatePassword();
+
+function validatePassword() {
+  if (password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+function logOutUser() {
+  var options = {
+    backgroundColor: '#ff0000',
+    timeout: false,
+    titleColor: '#ffffff',
+    messageColor: '#cecece',
+    icon: 'fas fa-exclamation-circle',
+    iconColor: '#ffffff',
+    close: false,
+    layout: 1,
+    title: 'Log Out?',
+    message: 'Are you sure?',
+    position: 'topCenter', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter
+    progressBar: false,
+    buttons: [
+        ['<button>Log out</button>', function (instance, toast) {
+          instance.hide({
+            transitionOut: 'fadeOutUp',
+        }, toast, 'buttonName');
+          Cookies.remove('user');
+          location.reload();
+        }], // true to focus
+        ['<button>Cancel</button>', function (instance, toast) {
+            instance.hide({
+                transitionOut: 'fadeOutUp',
+            }, toast, 'buttonName');
+        },true]
+    ]
+  }
+
+  iziToast.show(options);
+}
