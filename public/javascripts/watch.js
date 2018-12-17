4ea1bf37b75148c2ccff61589eb89250b2418eff
AevalAnkou <ankouaeval@gmail.com>
2018-12-17 18:14:00 -0500

    fixed twitch embed

---
+++ public/javascripts/watch.js
@@ -0,0 +1,9 @@
+var embed = new Twitch.Embed("twitch-embed", {
+    width: 1024,
+    height: 768,
+    channel: "vigridrgaming",
+    layout: "video-with-chat",
+    autoplay: true,
+    theme: "dark",
+    chat: "mobile"
+  });
